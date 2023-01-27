d7f4355c222e608cd2d6faac8e0622a27246b98c
dartixus <dschnurp@students.zcu.cz>
2023-01-28 00:35:39 +0100

    exploring dataset

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+import numpy as np
 from pandas import read_parquet
 from transformers import AutoTokenizer, AutoModel
 
+# https://drive.google.com/drive/folders/1JG6Fibvhs0Jz6JD83gwMqAmzUV9rsoX3
+# Reading the parquet file and storing it in a dataframe.
 data = read_parquet("SODD_dev.parquet.gzip")
+# Printing the number of rows in the dataframe, the first post, the second post, and the label.
 print(data.count())
-print(data.first_post[0])
+first_post = data.first_post[0]
+second_post = data.second_post[0]
+print(data.label[0])
 exit(0)
 
 
+# Loading the model and tokenizer from the HuggingFace model hub.
 tokenizer = AutoTokenizer.from_pretrained("UWB-AIR/MQDD-duplicates")
 model = AutoModel.from_pretrained("UWB-AIR/MQDD-duplicates")
 
 
+# Encoding the first post into a sequence of integers.
+encoding_first = tokenizer.encode_plus(first_post, return_tensors="pt")
+input_ids_first = encoding_first["input_ids"]
 
-question, text = "W", "W"
+# Encoding the second post into a sequence of integers.
+encoding_second = tokenizer.encode_plus(second_post, return_tensors="pt")
+input_ids_second = encoding_second["input_ids"]
 
-encoding = tokenizer.encode_plus(question, text, return_tensors="pt")
-input_ids = encoding["input_ids"]
+output1, output2 = model(input_ids_first, input_ids_second)
 
-
-start_scores, end_scores = model(input_ids)
-print(start_scores.shape)
-
-print(end_scores.shape)
-
-
-
+print(np.argmax(output1.detach().numpy()))
