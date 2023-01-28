c8809750e6d954fd6206b3e4a50caef124a32e49
dartixus <dschnurp@students.zcu.cz>
2023-01-28 13:41:54 +0100

    exploring dataset

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import numpy as np
 from pandas import read_parquet
 from transformers import AutoTokenizer, AutoModel
 
+from data.utils import sanitize_html_for_web
+
 # https://drive.google.com/drive/folders/1JG6Fibvhs0Jz6JD83gwMqAmzUV9rsoX3
 # Reading the parquet file and storing it in a dataframe.
 data = read_parquet("SODD_dev.parquet.gzip")
 # Printing the number of rows in the dataframe, the first post, the second post, and the label.
 print(data.count())
-first_post = data.first_post[0]
-second_post = data.second_post[0]
+first_post = sanitize_html_for_web(data.first_post[0])
+second_post = sanitize_html_for_web(data.second_post[0])
+
+print(second_post)
+
 print(data.label[0])
 exit(0)
 
@@ -19,13 +24,13 @@ model = AutoModel.from_pretrained("UWB-AIR/MQDD-duplicates")
 
 
 # Encoding the first post into a sequence of integers.
-encoding_first = tokenizer.encode_plus(first_post, return_tensors="pt")
-input_ids_first = encoding_first["input_ids"]
+encoding_first = tokenizer.encode(first_post, return_tensors="pt")
+
 
 # Encoding the second post into a sequence of integers.
-encoding_second = tokenizer.encode_plus(second_post, return_tensors="pt")
-input_ids_second = encoding_second["input_ids"]
+encoding_second = tokenizer.encode(second_post, return_tensors="pt")
 
-output1, output2 = model(input_ids_first, input_ids_second)
+
+output1, output2 = model(encoding_first, encoding_second)
 
 print(np.argmax(output1.detach().numpy()))
