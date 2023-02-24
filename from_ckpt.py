44d6404f58f8081ab165aeafcc33c5e384f773f2
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-02-24 10:02:27 +0100

    exploring dataset

--- from_ckpt.py
+++ from_ckpt.py
@@ -1,6 +1,8 @@
 import torch
 from MQDD_model import ClsHeadModelMQDD
 
+# https://github.com/kiv-air/MQDD
+
 model = ClsHeadModelMQDD("UWB-AIR/MQDD-duplicates")
 ckpt = torch.load("model.pt",  map_location="cpu")
 model.load_state_dict(ckpt["model_state"])
