b8eb0ea5fae3c70916c97c35b0af5d3f9835120c
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-04-11 12:23:08 +0200

    params to faiss and renaming todo eos_token -> sep , bos_token -> cls

--- data/index_config/faiss_indexer_config/cfg.py
+++ data/index_config/faiss_indexer_config/cfg.py
@@ -1,2 +1,12 @@
+import argparse
+import os
 
-input_folder = "../../../../../SERVER_APPS/gamedev.stackexchange.com/"
+parser = argparse.ArgumentParser(description='Simple indexer')
+parser.add_argument('-i', '--input_file_path',
+                    required=True)
+args = parser.parse_args()
+if not os.path.isfile(args.input_file_path):
+    raise "bad input_file_path..."
+
+input_folder = args.input_file_path
+
--- experiments/question_encoder.py
+++ experiments/question_encoder.py
@@ -3,14 +3,17 @@ import torch
 from transformers import AutoTokenizer
 from transformers import LongformerConfig, LongformerModel
 
+
 def encode_question(question):
     max_length = 512
 
     # Loading the tokenizer from the pretrained model.
     tokenizer = AutoTokenizer.from_pretrained("UWB-AIR/MQDD-duplicates")
+    tokenizer.bos_token = "cls"
+    tokenizer.eos_token = "sep"
     # Loading the pretrained model.
     model = LongformerModel.from_pretrained("UWB-AIR/MQDD-duplicates")
-    # tokenizer todo eos_token -> sep , bos_token -> cls
+    # tokenizer eos_token -> sep , bos_token -> cls
     # Encoding the question into a list of integers.
     encoded_question = tokenizer.encode(
         question,
@@ -26,13 +29,12 @@ def encode_question(question):
         return_token_type_ids=True,
         truncation="longest_first", return_tensors="pt")
 
-    encoded_question.data["input_ids"] = torch.reshape(encoded_question.data["input_ids"], (1, encoded_question.data["input_ids"].shape[0]))
+    encoded_question.data["input_ids"] = torch.reshape(encoded_question.data["input_ids"],
+                                                       (1, encoded_question.data["input_ids"].shape[0]))
     encoded_question.data["token_type_ids"] = torch.reshape(encoded_question.data["token_type_ids"],
-                                                       (1, encoded_question.data["token_type_ids"].shape[0]))
+                                                            (1, encoded_question.data["token_type_ids"].shape[0]))
     encoded_question.data["attention_mask"] = torch.reshape(encoded_question.data["attention_mask"],
-                                                       (1, encoded_question.data["attention_mask"].shape[0]))
+                                                            (1, encoded_question.data["attention_mask"].shape[0]))
     NECO = model(**encoded_question)
-
-
 
     return np.squeeze(encoded_question.detach().numpy())
