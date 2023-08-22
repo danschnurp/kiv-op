0a4191f1b407dcc0087b7c4ec8823c7c6abeff60
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-08-22 19:23:23 +0200

    fixes

--- web/search/search.py
+++ web/search/search.py
@@ -70,11 +70,9 @@ def __search_fulltext(search_text, search_code, post_start, post_end, request, d
             # gets siamese posts from vectored posts
             posts_search = []
 
-            for post_id in post_ids:
+            for post_id in post_ids[:10]:
                 try:
-                    post = Post.search().filter("term", post_ID=post_id).filter("match", page=page) \
-                        .filter("range", creation_date={"gte": date_filter["start"], "lt": date_filter["end"]}) \
-                        .execute()
+                    post = Post.search().filter("term", post_ID=post_id).filter("match", page=page).execute()
                     result_posts = Post.get_display_info_for_posts(post.hits)
                     posts_search.extend(result_posts)
                 except Exception:
